d9f168c104553c3e58b7eed94dedce5069a3ae39
Przemysław Niewiadowski <niewiadowski@gmail.com>
2017-06-23 05:55:48 +0200

    manynames

---
+++ manynames.py
@@ -0,0 +1,31 @@
+x = 11
+
+
+def f():
+    print(x)
+
+
+def g():
+    x = 22
+    print(x)
+
+
+class C:
+    x = 33
+
+    def m(self):
+        x = 44
+        self.x = 55
+
+if __name__ == '__main__':
+    print(x)
+    f()
+    g()
+    print(x)
+
+    obj=C()
+    print(obj.x)
+
+    obj.m()
+    print(obj.x)
+    print(C.x)
