6679ed1f58186b8d071e658c34de6f68bf52b8c3
Przemysław Niewiadowski <niewiadowski@gmail.com>
2017-06-23 06:41:48 +0200

    classtree

--- classscope.py
+++ classscope.py
@@ -4,12 +4,16 @@ def nester():
     x=2
     print(x)
     class C:
+        x=3
         print(x)
         def method1(self):
             print(x)
+            print(self.x)
         def method2(self):
-            x=3
+            x=4
             print(x)
+            self.x=5
+            print(self.x)
     i=C()
     i.method1()
     i.method2()
