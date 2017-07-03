9b1773d71e493a0c22663e1d447c59989df0a5a2
Przemysław Niewiadowski <niewiadowski@gmail.com>
2017-07-03 06:38:32 +0200

    commuters

--- commuter.py
+++ commuter.py
@@ -1,9 +1,46 @@
 class Commuter1:
-    def __init__(self,val):
+    def __init__(self, val):
         self.val = val
+
     def __add__(self, other):
         print('add', self.val, other)
         return self.val + other
+
     def __radd__(self, other):
         print('radd', self.val, other)
         return self.val + other
+
+
+class Commuter2:
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        print('add', self.val, other)
+        return self.val + other
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+
+class Commuter3:
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        print('add', self.val, other)
+        return self.val + other
+
+    def __radd__(self, other):
+        return self + other
+
+
+class Commuter4:
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        print('add', self.val, other)
+        return self.val + other
+
+    __radd__ = __add__
