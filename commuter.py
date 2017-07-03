c39ab3999aa5223810d6b233d58777d6f7688ffb
Przemysław Niewiadowski <niewiadowski@gmail.com>
2017-07-03 06:29:21 +0200

    commuter

---
+++ commuter.py
@@ -0,0 +1,9 @@
+class Commuter1:
+    def __init__(self,val):
+        self.val = val
+    def __add__(self, other):
+        print('add', self.val, other)
+        return self.val + other
+    def __radd__(self, other):
+        print('radd', self.val, other)
+        return self.val + other
