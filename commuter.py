32b0d0d62ffa794bcc9a54f9615e17bcb4d8e8cb
Przemysław Niewiadowski <niewiadowski@gmail.com>
2017-07-03 06:44:09 +0200

    commuter5

--- commuter.py
+++ commuter.py
@@ -44,3 +44,19 @@ class Commuter4:
         return self.val + other
 
     __radd__ = __add__
+
+
+class Commuter5:
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        if isinstance(other, Commuter5):
+            other = other.val
+        return Commuter5(self.val + other)
+
+    def __radd__(self, other):
+        return Commuter5(other, self.val)
+
+    def __str__(self):
+        return '<Commuter5: %s' % self.val
