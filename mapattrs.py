ab909c49de963b171cde2f1db9f9c1b253d4c89d
Przemysław Niewiadowski <niewiadowski@gmail.com>
2017-08-03 06:14:53 +0200

    mapattrs

--- mapattrs.py
+++ mapattrs.py
@@ -30,3 +30,82 @@ def inverdict(D):
 
     return {V: keysof(V) for V in set(D.values())}
 
+
+def dflr(cls):
+    """Classic depth-first left-to-right order of class tree at cls.
+    Cycles not possible: Python disallows on __bases__ changes."""
+    here = [cls]
+    for sup in cls.__bases__:
+        here += dflr(sup)
+    return here
+
+
+def inheritance(instance):
+    """Inheritance order squence: new-style (MRO) or classic (DFLR)"""
+    if hasattr(instance.__class__, '__mro__'):
+        return (instance,) + instance.__class__.__mro__
+    else:
+        return [instance] + dflr(instance.__class__)
+
+
+def mapattrs(instance, withobject=False, bysource=False):
+    """dict with keys giving all inherited attributes of instance,
+    with value giving the object that each is inherited from.
+    withobject: False=remove object built-in class attributes
+    bysource: True=group result by object instead of attributes.
+    Support classes with slots that preclude __dict__ in instances."""
+    attr2obj = {}
+    inherits = inheritance(instance)
+    for attr in dir(instance):
+        for obj in inherits:
+            if hasattr(obj, '__dict__') and attr in obj.__dict__:
+                attr2obj[attr] = obj
+                break
+    if not withobject:
+        attr2obj = filterdictvals(attr2obj, object)
+    return attr2obj if not bysource else inverdict(attr2obj)
+
+
+if __name__ == '__main__':
+    print('Classic classes in 2.X, new style in 3.X')
+
+
+    class A: attr1 = 1
+
+
+    class B(A): attr2 = 2
+
+
+    class C(A): attr1 = 3
+
+
+    class D(B, C): pass
+
+
+    I = D()
+    print('Py=>%s' % I.attr1)
+    trace(inheritance(I), 'INH\n')
+    trace(mapattrs(I), 'ATTRS\n')
+    trace(mapattrs(I, bysource=True), 'OBJS\n')
+
+    print('New-style classes in 2.X and 3.X')
+
+
+    class A(object): attr1 = 1
+
+
+    class B(A): attr2 = 2
+
+
+    class C(A): attr1 = 3
+
+
+    class D(B, C): pass
+
+
+    I = D()
+    print('Py=>%s' % I.attr1)
+
+    trace(inheritance(I), 'INH\n')
+    trace(mapattrs(I), 'ATTRS\n')
+    trace(mapattrs(I, bysource=True), 'OBJS\n')
