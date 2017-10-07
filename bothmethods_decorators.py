6adf7e7edc10054b14605a84401912bb6626df4f
Przemysław Niewiadowski <niewiadowski@gmail.com>
2017-10-07 20:46:45 +0200

    tracer

---
+++ bothmethods_decorators.py
@@ -0,0 +1,15 @@
+class Methods(object):
+    def imeth(self, x):
+        print([self, x])
+
+    @staticmethod
+    def smeth(x):
+        print(x)
+
+    @classmethod
+    def cmeth(cls, x):
+        print([cls, x])
+
+    @property
+    def name(self):
+        return 'Bob ' + self.__class__.__name__
