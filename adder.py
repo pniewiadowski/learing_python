f8f608dcd320f7a17ee52315095acab9514975f5
Przemysław Niewiadowski <niewiadowski@gmail.com>
2017-10-17 16:18:02 +0200

    adder

---
+++ adder.py
@@ -0,0 +1,24 @@
+class Adder:
+    def add(self, x, y):
+        print('Not implemented!')
+
+    def __init__(self, start=[]):
+        self.data = start
+
+    def __add__(self, other):
+        return self.add(self.data, other)
+
+
+class ListAdder(Adder):
+    def add(self, x, y):
+        return x + y
+
+
+class DictAdder(Adder):
+    def add(self, x, y):
+        new = {}
+        for k in x.keys():
+            new[k] = x[k]
+        for k in y.keys():
+            new[k] = y[k]
+        return new
