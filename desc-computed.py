9c2567b0086bf132204b3d7420bdfb74f49e4de7
Przemysław Niewiadowski <niewiadowski@gmail.com>
2017-11-18 21:30:49 +0100

    desc-computed

---
+++ desc-computed.py
@@ -0,0 +1,25 @@
+class DescSquare:
+    def __init__(self, start):
+        self.value = start
+
+    def __get__(self, instance, owner):
+        return self.value ** 2
+
+    def __set__(self, instance, value):
+        self.value = value
+
+
+class Client1:
+    X = DescSquare(3)
+
+
+class Client2:
+    X = DescSquare(32)
+
+
+c1 = Client1()
+c2 = Client2()
+print(c1.X)
+c1.X = 4
+print(c1.X)
+print(c2.X)
