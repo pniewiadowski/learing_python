882e6cb4f26c4b76946af024009ae2894d2fb176
Przemysław Niewiadowski <niewiadowski@gmail.com>
2017-12-03 15:33:23 +0100

    decorator5

---
+++ decorator5.py
@@ -0,0 +1,25 @@
+def tracer(func):
+    def wrapper(*args, **kwargs):
+        wrapper.calls += 1
+        print('call {} to {}'.format(wrapper.calls, func.__name__))
+        return func(*args, **kwargs)
+
+    wrapper.calls = 0
+    return wrapper
+
+
+@tracer
+def spam(a, b, c):
+    print(a + b + c)
+
+
+@tracer
+def eggs(x, y):
+    print(x ** y)
+
+
+spam(1, 2, 3)
+spam(a=4, b=5, c=6)
+
+eggs(2, 16)
+eggs(4, y=4)
