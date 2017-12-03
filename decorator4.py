83ac531c1df4f78a83be5b70e0556eb3b46f045f
Przemysław Niewiadowski <niewiadowski@gmail.com>
2017-12-03 15:21:03 +0100

    decorator3

---
+++ decorator4.py
@@ -0,0 +1,27 @@
+def tracer(func):
+    calls = 0
+
+    def wrapper(*args, **kwargs):
+        nonlocal calls
+        calls += 1
+        print('call {} to {}'.format(calls, func.__name__))
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@tracer
+def spam(a, b, c):
+    print(a + b + c)
+
+
+@tracer
+def eggs(x, y):
+    print(x ** y)
+
+
+spam(1, 2, 3)
+spam(a=4, b=5, c=6)
+
+eggs(2, 16)
+eggs(4, y=4)
