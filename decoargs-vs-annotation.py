cadd785e1777743330a0bdc8934184ffd16f7a54
niewiadowski@gmail.com <XgYP72Xu>
2018-02-06 21:21:23 +0100

    decoargs-vs-annotation

---
+++ decoargs-vs-annotation.py
@@ -0,0 +1,38 @@
+def rangetest(**argchecks):
+    def onDecorator(func):
+        def onCall(*pargs, **kargs):
+            print(argchecks)
+            for check in argchecks:
+                pass
+            return func(*pargs, **kargs)
+
+        return onCall
+
+    return onDecorator
+
+
+@rangetest(a=(1, 5), c=(0.0, 1.0))
+def func(a, b, c):
+    print(a + b + c)
+
+
+func(1, 2, c=3)
+
+
+def rangetest(func):
+    def onCall(*pargs, **kargs):
+        argchecks = func.__annotations__
+        print(argchecks)
+        for check in argchecks:
+            pass
+        return func(*pargs, **kargs)
+
+    return onCall
+
+
+@rangetest
+def func(a: (1, 5), b, c: (0.0, 1.0)):
+    print(a + b + c)
+
+
+func(1, 2, c=3)
