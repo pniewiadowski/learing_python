30a59dd8a10878947512f18a4f3ca30039deaec6
niewiadowski@gmail.com <XgYP72Xu>
2018-02-13 21:19:18 +0100

    metaclass4

---
+++ metaclass3.py
@@ -0,0 +1,22 @@
+def MetaFunc(classname, supers, classdict):
+    print('In MetaFunc: ', classname, supers, classdict, sep='\n...')
+    return type(classname, supers, classdict)
+
+
+class Eggs:
+    pass
+
+
+print('making class')
+
+
+class Spam(Eggs, metaclass=MetaFunc):
+    data = 1
+
+    def meth(self, arg):
+        return self.data + arg
+
+
+print('making instance')
+X = Spam()
+print('data:', X.data, X.meth(2))
