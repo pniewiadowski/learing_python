2610e0f8540c4689dad0863c026158101586cc02
niewiadowski@gmail.com <XgYP72Xu>
2018-02-13 21:31:37 +0100

    metaclass4-super

---
+++ metaclass4-super.py
@@ -0,0 +1,32 @@
+class SuperMetaObj:
+    def __call__(self, classname, supers, classdict):
+        print('In SuperMetaObj.call: ', classname, supers, classdict, sep='\n...')
+        Class = self.__New__(classname, supers, classdict)
+        self.__Init__(Class, classname, supers, classdict)
+        return Class
+
+
+class SubMetaObj(SuperMetaObj):
+    def __New__(self, classname, supers, classdict):
+        print('In SubMetaObj.new: ', classname, supers, classdict, sep='\n...')
+        return type(classname, supers, classdict)
+
+    def __Init__(self, Class, classname, supers, classdict):
+        print('In SubMetaObj.init:', classname, supers, classdict, sep='\n...')
+        print('...init class object:', list(Class.__dict__.keys()))
+
+
+class Eggs:
+    pass
+
+
+class Spam(Eggs, metaclass=SubMetaObj()):
+    data = 1
+
+    def meth(self, arg):
+        return self.data + arg
+
+
+print('making instance')
+X = Spam()
+print('data:', X.data, X.meth(2))
