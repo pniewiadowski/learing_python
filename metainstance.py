738c9819cc155faa51f913e97d8e27cfe953296f
niewiadowski@gmail.com <XgYP72Xu>
2018-02-14 21:16:24 +0100

    metainstance

---
+++ metainstance.py
@@ -0,0 +1,17 @@
+class MetaOne(type):
+    def __new__(meta, classname, supers, classdict):
+        print('In MetaOne.new:', classname)
+        return type.__new__(meta, classname, supers, classdict)
+
+    def toast(cls):
+        return 'toast'
+
+
+class Super(metaclass=MetaOne):
+    def spam(self):
+        return 'spam'
+
+
+class Sub(Super):
+    def eggs(self):
+        return 'eggs'
