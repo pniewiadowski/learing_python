ff743003be62cd24172755bba3271c6efe7a036e
niewiadowski@gmail.com <XgYP72Xu>
2018-02-15 20:33:32 +0100

    extended-deco

---
+++ extended-deco.py
@@ -0,0 +1,36 @@
+def eggsfunc(obj):
+    return obj.value * 4
+
+
+def hamfunc(obj, value):
+    return value + 'ham'
+
+
+def Extender(aClass):
+    aClass.eggs = eggsfunc
+    aClass.ham = hamfunc
+    return aClass
+
+
+@Extender
+class Client1:
+    def __init__(self, value):
+        self.value = value
+
+    def spam(self):
+        return self.value * 2
+
+
+@Extender
+class Client2:
+    value = 'ni?'
+
+
+X = Client1('Ni!')
+print(X.spam())
+print(X.eggs())
+print(X.ham('bacon'))
+
+Y = Client2()
+print(Y.eggs())
+print(Y.ham('bacon'))
