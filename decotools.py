dceb1d091f373115ee4ac49a51b949abd4247521
niewiadowski@gmail.com <XgYP72Xu>
2018-02-20 20:47:50 +0100

    decoall-manual

---
+++ decotools.py
@@ -0,0 +1,32 @@
+import time
+
+
+def tracer(func):
+    calls = 0
+
+    def onCall(*args, **kwargs):
+        nonlocal calls
+        calls += 1
+        print('call {} to {}'.format(calls, func.__name__))
+        return func(*args, **kwargs)
+
+    return onCall
+
+
+def timer(label='', trace=True):
+    def onDecorator(func):
+        def onCall(*args, **kargs):
+            start = time.clock()
+            result = func(*args, **kargs)
+            elapsed = time.clock() - start
+            onCall.alltime += elapsed
+            if trace:
+                format = '%s%s: %.5f, %.5f'
+                values = (label, func.__name__, elapsed, onCall.alltime)
+                print(format % values)
+            return result
+
+        onCall.alltime = 0
+        return onCall
+
+    return onDecorator
